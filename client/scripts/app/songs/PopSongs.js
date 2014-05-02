ce238ae95da4b9f3a863716fa48e6f6c1dc4016a
Jason Sigal <jason.sigal@gmail.com>
2014-05-02 18:11:51 -0400

    Update PopSongs.js
    added some pop songs, more on the way...

--- client/scripts/app/songs/PopSongs.js
+++ client/scripts/app/songs/PopSongs.js
@@ -5,91 +5,55 @@ define(function(){
 			artist : "Beyonce",
 			track : "Drunk in Love",
 			uri : "5EmCpD8tUj78VW3kgaEjME",
-			bpm : 120
+			bpm : 139.923
 		},
 		{
-			artist : "Beyonce",
-			track : "Drunk in Love",
-			uri : "5EmCpD8tUj78VW3kgaEjME",
-			bpm : 120
+			artist : "Blondie",
+			track : "Heart of Glass",
+			uri : "0a4agFmqHXxcZl1nho1BxM",
+			bpm : 114.586
 		},
 		{
-			artist : "Beyonce",
-			track : "Drunk in Love",
-			uri : "5EmCpD8tUj78VW3kgaEjME",
-			bpm : 120
+			artist : "The Whispers",
+			track : "And The Beat Goes On",
+			uri : "1kN4Pp1m4aIbLafnF9uTSc",
+			bpm : 122.998
 		},
 		{
-			artist : "Beyonce",
-			track : "Drunk in Love",
-			uri : "5EmCpD8tUj78VW3kgaEjME",
-			bpm : 120
+			artist : "Cheryl Lynn",
+			track : "Got To Be Real",
+			uri : "6iEFcZI1KxxPaAGlnOVn3y",
+			bpm : 114.758
 		},
 		{
-			artist : "Beyonce",
-			track : "Drunk in Love",
-			uri : "5EmCpD8tUj78VW3kgaEjME",
-			bpm : 120
+			artist : "Michael Jackson",
+			track : "Billie Jean",
+			uri : "5ChkMS8OtdzJeqyybCc9R5",
+			bpm : 117.067
 		},
 		{
-			artist : "Beyonce",
-			track : "Drunk in Love",
-			uri : "5EmCpD8tUj78VW3kgaEjME",
-			bpm : 120
+			artist : "Marcia Griffiths",
+			track : "Electric Boogie",
+			uri : "1JyaAeaXVFnVv5ikwWQVQ4",
+			bpm : 108.136
 		},
 		{
-			artist : "Beyonce",
-			track : "Drunk in Love",
-			uri : "5EmCpD8tUj78VW3kgaEjME",
-			bpm : 120
+			artist : "Eddy Grant",
+			track : "Electric Avenue",
+			uri : "4cqUq7n3ad0DhJ2JCVw34v",
+			bpm : 121.936
 		},
 		{
-			artist : "Beyonce",
-			track : "Drunk in Love",
-			uri : "5EmCpD8tUj78VW3kgaEjME",
-			bpm : 120
+			artist : "Mariah Carey",
+			track : "Fantasy",
+			uri : "6xkryXuiZU360Lngd4sx13",
+			bpm : 102.395
 		},
 		{
-			artist : "Beyonce",
-			track : "Drunk in Love",
-			uri : "5EmCpD8tUj78VW3kgaEjME",
-			bpm : 120
-		},
-		{
-			artist : "Beyonce",
-			track : "Drunk in Love",
-			uri : "5EmCpD8tUj78VW3kgaEjME",
-			bpm : 120
-		},
-		{
-			artist : "Beyonce",
-			track : "Drunk in Love",
-			uri : "5EmCpD8tUj78VW3kgaEjME",
-			bpm : 120
-		},
-		{
-			artist : "Beyonce",
-			track : "Drunk in Love",
-			uri : "5EmCpD8tUj78VW3kgaEjME",
-			bpm : 120
-		},
-		{
-			artist : "Beyonce",
-			track : "Drunk in Love",
-			uri : "5EmCpD8tUj78VW3kgaEjME",
-			bpm : 120
-		},
-		{
-			artist : "Beyonce",
-			track : "Drunk in Love",
-			uri : "5EmCpD8tUj78VW3kgaEjME",
-			bpm : 120
-		},
-		{
-			artist : "Beyonce",
-			track : "Drunk in Love",
-			uri : "5EmCpD8tUj78VW3kgaEjME",
-			bpm : 120
+			artist : "MGMT",
+			track : "Electric Feel",
+			uri : "3FtYbEfBqAlGO46NUDQSAt",
+			bpm : 103.013
 		},
 	];
 
