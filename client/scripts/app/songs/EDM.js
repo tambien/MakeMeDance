4d0d0d06242d04bed42f4bd787815a89acc31087
Jason Sigal <jason.sigal@gmail.com>
2014-05-02 19:30:06 -0400

    added some tracks, more to come!

--- client/scripts/app/songs/EDM.js
+++ client/scripts/app/songs/EDM.js
@@ -2,28 +2,22 @@ define(function(){
 
 	var EDMSongs = [
 		{
-			artist : "Tim Hecker",
-			track : "Sketch 3",
-			uri : "3sfQoqjzyrDpI8NXlZ2ddX",
-			bpm : 120
+			artist : "Daft Punk",
+			track : "One More Time",
+			uri : "0DiWol3AO6WpXZgp0goxAV",
+			bpm : 122.79
 		},
 		{
-			artist : "Tim Hecker",
-			track : "Sketch 3",
-			uri : "3sfQoqjzyrDpI8NXlZ2ddX",
-			bpm : 120
+			artist : "Ellie Goulding",
+			track : "Goodness Gracious (Chainsmokers Extended Remix)",
+			uri : "4gjrsP1Tmrqh1IiHhMXQm5",
+			bpm : 128.014
 		},
 		{
-			artist : "Tim Hecker",
-			track : "Sketch 3",
-			uri : "3sfQoqjzyrDpI8NXlZ2ddX",
-			bpm : 120
-		},
-		{
-			artist : "Tim Hecker",
-			track : "Sketch 3",
-			uri : "3sfQoqjzyrDpI8NXlZ2ddX",
-			bpm : 120
+			artist : "Buraka Som Sistema",
+			track : "Aqui Para Voces",
+			uri : "4t3mArDLqiOnrHLNhDiNS8",
+			bpm : 135.156
 		},
 		{
 			artist : "Tim Hecker",
