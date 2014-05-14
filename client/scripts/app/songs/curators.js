0ef81f5b1f83468ad64dff996ac110f1ec9ee10a
Jason Sigal <jason.sigal@gmail.com>
2014-05-14 01:14:02 -0400

    Update curators.js

--- client/scripts/app/songs/curators.js
+++ client/scripts/app/songs/curators.js
@@ -17,15 +17,19 @@ define(function(){
 			songs : ["2YLmyVJXOe4KRI3KbSXNOi", "3mRjerKQVtccyYnoCfWOS9", "55cU1KvvGdJEwPY4gAT3pJ", "4UB6cBHBeVb0JkIzYjMW2H", "1YDOQZw36sklFHS0ZdK8c3", "2hQCzcb3qyZirWzOD5Yzoj", "7cyUppcNj7ojpzPlB0CPW9", "1a3iWvMF1FJJZ9GvhtQNTc", "2073QOEC8rBtSyTsRyaWiP"]
 		},
 		{
-		name : "Omer",
-		spotifyuser: "omershapira",
-		songs: ["1W8Jl9KaSSt23ihF1Mej17", "1Bm7pEL9YECCwg2YKueqAR", "14WrYIFpHTUMIdjlJbTA6W", "4SeJWEQfmmsPEDB9JP7HBa", "02v5X54nrYYGL78zQF7O7l", "5j6H5de3TPYrw4sTjGHKAY"]
-	},
-	{
-		name: "Erin",
-		spotifyuser: "erinmikulenka",
-		songs: ["7rnAXmOwAQkn2nZaae1Xfs", "1XbNKrlzXgZZIykfACTAoR", "1XbNKrlzXgZZIykfACTAoR", "3sl4dcqSwxHVnLfqwF2jly", "0Hf4aIJpsN4Os2f0y0VqWl", "0Hf4aIJpsN4Os2f0y0VqWl", "6YQCMUdW7FxMthpE5Iz5Z7"]
-	}
+			name : "Omer",
+			spotifyuser: "omershapira",
+			songs: ["1W8Jl9KaSSt23ihF1Mej17", "1Bm7pEL9YECCwg2YKueqAR", "14WrYIFpHTUMIdjlJbTA6W", "4SeJWEQfmmsPEDB9JP7HBa", "02v5X54nrYYGL78zQF7O7l", "5j6H5de3TPYrw4sTjGHKAY"]
+		},
+		{
+			name: "Erin",
+			spotifyuser: "erinmikulenka",
+			songs: ["7rnAXmOwAQkn2nZaae1Xfs", "1XbNKrlzXgZZIykfACTAoR", "1XbNKrlzXgZZIykfACTAoR", "3sl4dcqSwxHVnLfqwF2jly", "0Hf4aIJpsN4Os2f0y0VqWl", "0Hf4aIJpsN4Os2f0y0VqWl", "6YQCMUdW7FxMthpE5Iz5Z7"]
+		},
+		{
+			name: "Jerllin",
+			spotifyuser: "1239272058",
+			songs: ["2nMeu6UenVvwUktBCpLMK9", "439TlnnznSiBbQbgXiBqAd", "69yVxyuRahEzs2taFMVVoO", "2cGxRwrMyEAp8dEbuZaVv6", "44eQkbofZeY7rEPnPdobwa", "3gbBpTdY8lnQwqxNCcf795"]
 
 	];
 
